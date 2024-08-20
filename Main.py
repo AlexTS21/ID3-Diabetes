da2cfd1fdaa2e96d7767a774eec7d5d0d94684d0
Alejandro Tejeda <alejanafs28@gmail.com>
2024-08-19 19:26:38 -0600

    Se hizo una interfaz con tkinter funcional

--- Main.py
+++ Main.py
@@ -3,7 +3,6 @@ import data_stract
 import IDThree
 import Node
 
-print("------------------------------ID3------------------------------")
 #Abrir la base de datos
 file_path = "BD.xls"
 train_sheet = "E"
@@ -12,29 +11,15 @@ test_sheet = "P"
 
 #Extraemos la data de entrenamiento
 data = data_stract.get_Data(file_path, train_sheet)
-#print(data)
+
 #Extraemos la data de prueba
 dataP = data_stract.get_testData(file_path, test_sheet)
 #print(dataP)
 
 #Generamos el arbol de desicion
 tree = decition_tree.generate_binary_decition_tree(data)
-#Imprimimos el arbol de desicion
-print("\nEl arbol de desicion en post-orden:")
-#tree.printTree()
 
-#Calculamos la eficacia con los datos de prueba
-#result = []
-#correcrto = 0
-#for element in dataP:
-#    test = tree.test_path(element[1])
-#    result.append(test)
-#    if element[0] == test:
-#       correcrto+=1
-#
-#print("------------------------------\nLA EFICIENCIA ES: ", correcrto/len(dataP)*100, "%\nAciertos/total: ",  correcrto, "/", len(dataP))
-
-
+#Preguntas
 questions = [
     "FEMENINO (SI) MASCULINO (NO)"
     "Tienes emisión de volumen excesivo de orina",
@@ -54,40 +39,90 @@ questions = [
     "-50 (SI) +=50 (NO)"
 ]
 
-#tree.addQ(questions)
-tree.printTree()
 
-ls = [1,1,1]
+
+
+import tkinter as tk
+
+
+
 node = tree.root
-while node.left != None:
-    index, dat = IDThree.obtener_entropia_minima(node.data)
-    print(questions[index-1])
-    answ = int(input())
-    
-    if answ == 1:
-        node = node.left
-    else:
-        node = node.right
-    
 index, dat = IDThree.obtener_entropia_minima(node.data)
-print(questions[index-1])
-clas = Node.Node.getClass(node.data[index])
 
-answ = int(input())
+# Función que será llamada al presionar el botón
+def mi_funcion(answ):
+    global node
+    if node.left != None:
+        if answ == 1:
+            node = node.left
+        else:
+            node = node.right
+        index, dat = IDThree.obtener_entropia_minima(node.data)
+        label.config(text=questions[index-1])
+    else:
+        index, dat = IDThree.obtener_entropia_minima(node.data)
+        clas = Node.Node.getClass(node.data[index])
+        if clas == "GREEN":
+            strConf = "Test positive"
+        elif clas == "RED":
+            strConf = "Test negative"
+        elif clas == "BLUE":
+            if answ ==1:
+                strConf = "Test positive"
+            else:
+                strConf = "Test negative"
+        elif clas == "VIOLET":
+            if answ ==0:
+                strConf = "Test positive"
+            else:
+                strConf = "Test negative"
+        else:
+           strConf = "EL SISTEMA NO PUEDE CLASIFICAR ESTA ENTRADA"
+        label.config(text=strConf)
+        button1.config(state=tk.DISABLED)
+        button2.config(state=tk.DISABLED)
+        buttonE.config(state=tk.NORMAL)
+    return
+        
+def reboot():
+    global node
+    node = tree.root
+    
+    buttonS.config(state=tk.NORMAL)
+    buttonE.config(state=tk.DISABLED)
+    label.config(text=questions[index-1])
+    return
 
-if clas == "GREEN":
-    print("Test positive")
-elif clas == "RED":
-    print("Test negative")
-elif clas == "BLUE":
-    if answ ==1:
-        print("Test positive")
-    else:
-        print("Test negative")
-elif clas == "VIOLET":
-    if answ ==0:
-        print("Test positive")
-    else:
-        print("Test negative")
-else:
-   print("EL SISTEMA NO PUEDE CLASIFICAR ESTA ENTRADA")
+def start():
+    button1.config(state=tk.NORMAL)
+    button2.config(state=tk.NORMAL)
+    buttonS.config(state=tk.DISABLED)
+    label.config(text=questions[index-1])
+    return
+
+# Crear la ventana principal
+root = tk.Tk()
+root.title("Interfaz con funciones")
+root.geometry("400x230")
+
+# Crear una etiqueta (leyenda)
+label = tk.Label(root, text="Sistema de deteccion de diabetes", font=("Arial", 14))
+label.pack(pady=20)
+
+# Crear los botones y asociarlos con la función y sus parámetros
+button1 = tk.Button(root, text="Botón 1", width=10, command=lambda: mi_funcion( 1))
+button1.pack(pady=5)
+button1.config(state=tk.DISABLED)
+
+button2 = tk.Button(root, text="Botón 2", width=10, command=lambda: mi_funcion(0))
+button2.pack(pady=5)
+button2.config(state=tk.DISABLED)
+
+buttonE = tk.Button(root, text="Regresar al inicio", width=10, command=reboot)
+buttonE.pack(pady=8)
+buttonE.config(state=tk.DISABLED)
+
+buttonS  = tk.Button(root, text="Empezar Test", width=10, command=start)
+buttonS.pack(pady=8)
+# Ejecutar el bucle principal de la ventana
+root.mainloop()
